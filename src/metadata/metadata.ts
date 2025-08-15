b3fb7eb05fa0d321df1184386b0ce3bbcb442c7a
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-08-14 23:40:24 -0500

    add: popups, files for styles images metada, link sections, favicon

---
+++ src/metadata/metadata.ts
@@ -0,0 +1,16 @@
+const metadata = {
+  title: "malsebasmal Socials Links",
+  description:
+    "Todos mis links en un solo lugar.",
+  canonical: "https://fivebrains.pages.dev",
+  og: {
+    title: "malsebasmal Socials Links",
+    description:
+      "Todos mis links en un solo lugar",
+    url: "https://fivebrains.pages.dev",
+    image: 'https://fivebrains.pages.dev/assets/og-image.png',
+    type: 'website'
+  },
+} as const
+
+export default metadata
