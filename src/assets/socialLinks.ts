8cfb67b3f3554c68bcd72c0c1ef40db7de1608b2
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-08-20 16:18:44 -0500

    add: .ts file for links

---
+++ src/assets/socialLinks.ts
@@ -0,0 +1,60 @@
+import Instagram from "@images/icons/Instagram.astro"
+import Threads from "@images/icons/Threads.astro"
+import Pinterest from "@images/icons/Pinterest.astro"
+import Spotify from "@images/icons/Spotify.astro"
+import SubStack from "@images/icons/SubStack.astro"
+import Tumblr from "@images/icons/Tumblr.astro"
+import XSocial from "@images/icons/XSocial.astro"
+import Reddit from "@images/icons/Reddit.astro"
+import Mastodon from "@images/icons/Mastodon.astro"
+
+
+const SocialLinks = [
+  {
+    icon: Instagram,
+    link: "https://instagram.com/malsebasmal",
+    name: "malsebasmal"
+  },
+  {
+    icon: Threads,
+    link: "https://threads.net/@sebasmalonce",
+    name: "sebasmalonce"
+  },
+  {
+    icon: Pinterest,
+    link: "https://pinterest.com/malojosmal",
+    name: "malojosmal"
+  },
+  {
+    icon: Spotify,
+    link: "https://open.spotify.com/user/malferiasmal",
+    name: "malferiasmal"
+  },
+  {
+    icon: SubStack,
+    link: "https://substack.com/@malsebasmal",
+    name: "malsebasmal"
+  },
+  {
+    icon: Tumblr,
+    link: "https://pinkisbadismal.tumblr.com",
+    name: "pinkisbadismal"
+  },
+  {
+    icon: XSocial,
+    link: "https://x.com/malsebasmal",
+    name: "malsebasmal"
+  },
+  {
+    icon: Reddit,
+    link: "https://reddit.com/user/malsebasmal",
+    name: "malsebasmal"
+  },
+  {
+    icon: Mastodon,
+    link: "https://mastodon.social/@malsebasmal",
+    name: "malsebasmal"
+  },
+]
+
+export default SocialLinks
